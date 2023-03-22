8902800b12f3c2bf2baed10c8fa5d81375168a26
vahvake <62074587+vahvake@users.noreply.github.com>
2023-03-22 16:40:19 +0200

    Create pose detection

---
+++ src/lib/PoseDetection.svelte
@@ -0,0 +1,184 @@
+<script>
+  // https://selvamsubbiah.com/pose-detection-using-blazepose-in-tensorflow-js/
+
+  import * as poseDetection from '@tensorflow-models/pose-detection';
+  import '@tensorflow/tfjs-backend-webgl';
+  import { onMount } from 'svelte';
+
+  let video
+  let canvas
+  let ctx
+  let detector, model;
+  const scoreThreshold = 0.6;
+
+  let last_keypoints = []
+
+  onMount( () => {
+    video = document.getElementById('video');
+    canvas = document.getElementById('output');
+    ctx = canvas.getContext('2d');
+    app();
+  })
+
+  async function createDetector() {
+      model = poseDetection.SupportedModels.BlazePose;
+      const detectorConfig = {
+          runtime: "tfjs",
+          enableSmoothing: true,
+          modelType: "full"
+      };
+      detector = await poseDetection.createDetector(model, detectorConfig);
+  }
+
+  async function activateVideo() {
+      if(navigator.mediaDevices.getUserMedia) {
+          navigator.mediaDevices.getUserMedia({'video': {
+              width: '640',
+              height: '480'
+          }}).then(stream => {
+                  video.srcObject = stream;
+              })
+              .catch(e => {
+                  console.log("Error occurred while getting the video stream");
+              });
+      }
+      
+      video.onloadedmetadata = () => {
+          const videoWidth = video.videoWidth;
+          const videoHeight = video.videoHeight;
+
+          video.width = videoWidth;
+          video.height = videoHeight;
+          canvas.width = videoWidth;
+          canvas.height = videoHeight;  
+          
+          // Because the image from camera is mirrored, need to flip horizontally.
+          ctx.translate(videoWidth, 0);
+          ctx.scale(-1, 1);
+      };
+
+      video.addEventListener("loadeddata", predictPoses);   
+  }
+
+  async function predictPoses() {
+      let poses = null;
+      
+      if (detector != null) {
+          try {
+              poses = await detector.estimatePoses(video, { 
+                  flipHorizontal: false 
+              });
+          } catch (error) {
+              detector.dispose();
+              detector = null;
+              alert(error);
+          }
+      }
+
+      ctx.drawImage(video, 0, 0, video.videoWidth, video.videoHeight);
+
+      if (poses && poses.length > 0) {
+          for (const pose of poses) {
+              if (pose.keypoints != null) {
+                  //drawKeypoints(pose.keypoints);
+                  drawKeypointsColor(pose.keypoints);
+                  drawSkeleton(pose.keypoints);
+                  last_keypoints = pose.keypoints3D
+              }
+          }
+      }
+
+      window.requestAnimationFrame(predictPoses);
+  }
+
+  function drawKeypoints(keypoints) {
+      ctx.fillStyle = 'Green';
+      ctx.strokeStyle = 'White';
+      ctx.lineWidth = 2;
+      for(let i=0; i<keypoints.length; i++) {
+          drawKeypoint(keypoints[i]);    
+      }
+  }
+
+  function drawKeypoint(keypoint) {
+      const radius = 4;
+      if (keypoint.score >= scoreThreshold) {
+        const circle = new Path2D();
+        circle.arc(keypoint.x, keypoint.y, radius, 0, 2 * Math.PI);
+        ctx.fill(circle);
+        ctx.stroke(circle);
+      }
+  }
+
+  function drawKeypointsColor(keypoints) {
+      const keypointInd = poseDetection.util.getKeypointIndexBySide(model);
+      ctx.strokeStyle = 'White';
+      ctx.lineWidth = 2;
+      ctx.fillStyle = 'Red';
+      for (const i of keypointInd.middle) {
+          drawKeypoint(keypoints[i]);
+      }
+      ctx.fillStyle = 'Green';
+      for (const i of keypointInd.left) {
+          drawKeypoint(keypoints[i]);
+      }
+      ctx.fillStyle = 'Orange';
+      for (const i of keypointInd.right) {
+          drawKeypoint(keypoints[i]);
+      }
+  }
+
+  function drawSkeleton(keypoints) {
+      const color = "#fff";
+      ctx.fillStyle = color;
+      ctx.strokeStyle = color;
+      ctx.lineWidth = 2;
+
+      poseDetection.util.getAdjacentPairs(model)
+          .forEach(([i, j]) => {
+              const kp1 = keypoints[i];
+              const kp2 = keypoints[j];
+              if (kp1.score >= scoreThreshold && kp2.score >= scoreThreshold) {
+                  ctx.beginPath();
+                  ctx.moveTo(kp1.x, kp1.y);
+                  ctx.lineTo(kp2.x, kp2.y);
+                  ctx.stroke();
+              }
+      });
+  }
+
+  async function app() {
+      //Load the model and create a detector object
+      await createDetector();
+
+      //Enable camera and activate video
+      await activateVideo();
+  };
+</script>
+
+<div class="canvas-wrapper">
+  <canvas id="output"></canvas>
+  <video id="video" autoplay playsinline style="
+      -webkit-transform: scaleX(-1);
+      transform: scaleX(-1);
+      display: none">
+      <track default
+           kind="captions"
+           srclang="en"
+           src="/">
+  </video>
+  {#each last_keypoints as keypoint}
+    <div class="output">
+      <span class="name">{ keypoint.name}</span>  
+      <!-- X: across the sideways, increases leftwards -->
+      <span class="x">X: {parseInt( keypoint.x * 100 )}</span>  
+      <!-- Y: Height, increases downwards -->
+      <span class="y">Y: {parseInt( keypoint.y * 100 )}</span> 
+      <!-- Z: depth i quess but doesnt really work -->
+      <span class="x">Z: {parseInt( keypoint.z * 100 ) }</span>  
+      <!-- Score: the visibility of teh joint over 0.8 is good to use -->
+      <span class="x">Score: { keypoint.score }</span>  
+    </div>
+  {/each}
+</div>
+
