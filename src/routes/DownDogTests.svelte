faa7d942b945f703387a10a2d29aa6f942345e2e
vahvake <62074587+vahvake@users.noreply.github.com>
2023-03-27 19:52:04 +0300

    Make instruction lines follow the tests

--- src/routes/DownDogTests.svelte
+++ src/routes/DownDogTests.svelte
@@ -6,8 +6,18 @@
 
   // Everything on cam
   let onCam = new Posecheck( () => {
+    let critical_scores = [
+      //Wrists
+      Math.max( $Pose.keypoints[15].score, $Pose.keypoints[16].score ),
+      //Foot
+      Math.max( $Pose.keypoints[31].score, $Pose.keypoints[32].score ),
+      //Hips
+      Math.max( $Pose.keypoints[23].score, $Pose.keypoints[24].score ),
+      //Shoulders
+      Math.max( $Pose.keypoints[11].score, $Pose.keypoints[12].score ),
+    ]
     let visibility_scores = $Pose.keypoints.map( p => p.score )
-    return Math.min(...visibility_scores) > 0.6
+    return Math.min(...critical_scores) > 0.8
   })
 
   // Limbs on floor
@@ -21,12 +31,16 @@
       $Pose.keypoints[31].y,
       $Pose.keypoints[32].y,
     ]
-    return Math.max(...heights) - Math.min(...heights) < 0.2
+    return Math.max(...heights) - Math.min(...heights) < 0.2 && onCam.pass
   })
 
   // Butt up
   // Are hips 50cm over the feet
   let buttUp = new Posecheck( () => {
+    let shoullderHeights = [
+      $Pose.keypoints[11].y,
+      $Pose.keypoints[12].y,
+    ]
     let footHeights = [
       // Foot     
       $Pose.keypoints[31].y,
@@ -37,7 +51,7 @@
       $Pose.keypoints[23].y,
       $Pose.keypoints[24].y, 
     ]
-    return Math.max(...footHeights) - Math.min(...buttHeights) > 0.5 
+    return Math.max(...shoullderHeights) > Math.min(...buttHeights) && limbsDown.pass
   })
 
   let checkVals = []
@@ -65,18 +79,24 @@
 
 <section>
 
-    <!-- https://www.masterclass.com/articles/downward-dog-explained -->
- 
-
-  <Voiceline txt={'Welcome to voice yoga'}/>
-  
-  <Voiceline txt={'1. Get down on all fours. Start on the floor with your hands shoulder-width apart, with your shoulders above your wrists.'}/>
-  <Voiceline txt={'2. Lift your knees. Next, tuck your toes against the mat or ground, using that leverage to extend your legs and lift both knees into the air.'}/>
-  <Voiceline txt={'3. Extend. Extend and lengthen your spine, simultaneously pressing through the palms of your hands and balls of your feet.'}/>
-
   {#each checkVals as val}
     <div style="background-color: {val ? 'green' : 'red'};"></div>    
   {/each}
+
+  <Voiceline txt={'Welcome to voice yoga'}/>
+  <Voiceline txt={'Move in front of the device, so that you are fully in the picture'}/>
+  {#if checkVals[0]}
+    <Voiceline txt={'Get to all fours by plasing your hands on the floor in front of you, shoulder with apart'}/>
+  {/if}
+
+  {#if checkVals[1] && checkVals[0]}
+    <Voiceline txt={'Good! Now push trough your legs and lift yout hips up high'}/>
+  {/if}
+
+  {#if checkVals[2] && checkVals[1] && checkVals[0]}
+    <Voiceline txt={'You are now in downwards facing dog. Hold the postion and breth in and out'}/>
+  {/if}
+
 
 </section>
 
