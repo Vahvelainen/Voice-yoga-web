f5ebf57c8c1994b6e868959073d088611d9595ee
vahvake <62074587+vahvake@users.noreply.github.com>
2023-03-30 10:28:07 +0300

    Iterate on pose checks

--- src/routes/poses/DownDogTests.svelte
+++ src/routes/poses/DownDogTests.svelte
@@ -1,28 +1,28 @@
 <script>
-  import Posecheck from '@lib/PoseCheck'
   import Pose from '@stores/poseStore' 
-  import { onMount } from 'svelte';
   import Voiceline from '@lib/Voiceline.svelte';
+  import { onMount } from 'svelte';
+  
+  //Test stay true for 2 seconds after turning false
+  const timeout = 2000
 
   // Everything on cam
-  let onCam = new Posecheck( () => {
-    let critical_scores = [
-      //Wrists
-      Math.max( $Pose.keypoints[15].score, $Pose.keypoints[16].score ),
-      //Foot
-      Math.max( $Pose.keypoints[31].score, $Pose.keypoints[32].score ),
-      //Hips
-      Math.max( $Pose.keypoints[23].score, $Pose.keypoints[24].score ),
-      //Shoulders
-      Math.max( $Pose.keypoints[11].score, $Pose.keypoints[12].score ),
-    ]
-    let visibility_scores = $Pose.keypoints.map( p => p.score )
-    return Math.min(...critical_scores) > 0.8
-  })
-
-  // Limbs on floor
-  // Check that height of wrists and foots are max 20cm different from each other
-  let limbsDown = new Posecheck( () => {
+  let onCam  = false
+  let onCamTime
+  function onCamCheck() {
+    // onCam check moved to poseStore.js
+    if ( $Pose.onFrame ) {
+      onCam  = true
+      onCamTime = Date.now()
+    } else if ( Date.now() - onCamTime > timeout) {
+      onCam  = false
+    }
+  }
+  
+  let limbsDown = false
+  let limbsDownTime
+  function limbsDownCheck() {
+    // Check that height of wrists and foots are max 20cm different from each other
     let heights = [
       // Wrists
       $Pose.keypoints[15].y,
@@ -31,68 +31,67 @@
       $Pose.keypoints[31].y,
       $Pose.keypoints[32].y,
     ]
-    return Math.max(...heights) - Math.min(...heights) < 0.2 && onCam.pass
-  })
+    if ( Math.max(...heights) - Math.min(...heights) < 0.2 && onCam ) {
+      limbsDown = true
+      limbsDownTime = Date.now()
+    } else if ( Date.now() - limbsDownTime > timeout) {
+      limbsDown = false
+    }
+  }
 
-  // Butt up
-  // Are hips 50cm over the feet
-  let buttUp = new Posecheck( () => {
+  let buttUp = false
+  let buttUpTime
+  function buttUpCheck() {
+    // Are hips 50cm over the feet
     let shoullderHeights = [
       $Pose.keypoints[11].y,
       $Pose.keypoints[12].y,
-    ]
-    let footHeights = [
-      // Foot     
-      $Pose.keypoints[31].y,
-      $Pose.keypoints[32].y,
     ]
     let buttHeights = [
       // Hips
       $Pose.keypoints[23].y,
       $Pose.keypoints[24].y, 
     ]
-    return Math.max(...shoullderHeights) > Math.min(...buttHeights) && limbsDown.pass
-  })
-
-  let checkVals = []
-
-  function update() {
-    onCam.check()
-    limbsDown.check()
-    buttUp.check()
-
-    // svelte cant follow object values automatically
-    checkVals = [
-      onCam.pass,
-      limbsDown.pass,
-      buttUp.pass
-    ]
+    if (Math.max(...shoullderHeights) > Math.min(...buttHeights) && limbsDown) {
+      buttUp = true
+      buttUpTime = Date.now()
+    } else if ( Date.now() - buttUpTime > timeout) {
+      buttUp = false
+    }
   }
 
-  onMount( () => {
-    window.setInterval(
-      update,
-      100
-    )
+  function update() {
+    onCamCheck()
+    limbsDownCheck()
+    buttUpCheck()
+  }
+
+  //check done every 100ms
+  onMount( ()=>{
+    setInterval(update, 100)
   })
+
 </script>
 
 <section>
 
-  {#each checkVals as val}
-    <div style="background-color: {val ? 'green' : 'red'};"></div>    
-  {/each}
+  <div style="background-color: {onCam ? 'green' : 'red'};"></div>   
+  <div style="background-color: {limbsDown ? 'green' : 'red'};"></div>    
+  <div style="background-color: {buttUp ? 'green' : 'red'};"></div>    
+ 
+  {#if !onCam}
+    <Voiceline txt={'Move in front of the device, so that you are fully in the picture'}/>
+  {/if}
 
-  <Voiceline txt={'Move in front of the device, so that you are fully in the picture'}/>
-  {#if checkVals[0]}
+  {#if onCam && !limbsDown }
     <Voiceline txt={'Get to all fours by plasing your hands on the floor in front of you, shoulder with apart'}/>
   {/if}
 
-  {#if checkVals[1] && checkVals[0]}
+  {#if limbsDown && !buttUp}
     <Voiceline txt={'Good! Now push trough your legs and lift yout hips up high'}/>
   {/if}
 
-  {#if checkVals[2] && checkVals[1] && checkVals[0]}
+  {#if buttUp}
     <Voiceline txt={'You are now in downwards facing dog. Hold the postion and breth in and out'}/>
   {/if}
 
@@ -102,8 +101,7 @@
 <style>
   div {
     display: inline-block;
-    height: 10em;
+    height: 2em;
     width: 10em;
-    margin-right: 1em;
   }
 </style>
