1b41ba8fd32452345dfce55e5b7bb396d73dad8c
vahvake <62074587+vahvake@users.noreply.github.com>
2023-04-21 14:22:09 +0300

    Create class for making pose-checks
    PoseCheck = One test
    PoseCheckList = Wrapper that shows one test at a time

---
+++ src/lib/PoseCheck.svelte
@@ -0,0 +1,56 @@
+<script>
+  import { onMount } from "svelte"
+
+  let pass = false
+  let last_pass = 0
+  let last_failed = 0
+  export let test = () => false
+  export let timeout = 2000
+  export let pass_timeout = 100
+  export let active = true //Active by default but PoseCheckList will set it to false
+
+  let elem
+
+  const observer = new MutationObserver(setActive);
+  function setActive() {
+    active = elem? elem.getAttribute('active') == 'true' : false
+  }
+
+  const checkTest = () => {
+    const now = Date.now()
+    if ( test() ) {
+      last_pass = now
+      if ( now - last_failed > pass_timeout) { //will pass the first time the function is ran withouth timeout
+        pass = true
+      }
+    } else {
+      last_failed = now
+      if ( now - last_pass > timeout) {
+        pass = false
+      }
+    }
+  }
+
+  onMount( () => {
+    checkTest()
+    setInterval(checkTest, 100)
+    observer.observe( elem, { attributes: true } )
+  })
+
+</script>
+
+<div bind:this={elem} pass={pass} active={active}>
+  <!-- The divider should be hided when not in developement -->
+  <div class="pass_mark" style="background-color:{ pass? 'green' : 'red' };"></div>
+  {#if active}
+    <slot></slot>
+  {/if}
+</div>
+
+<style>
+  .pass_mark {
+    height: 2em;
+    width: 2em;
+    display: inline-block;
+  }
+</style>
---
+++ src/lib/PoseCheckList.svelte
@@ -0,0 +1,52 @@
+<script>
+  import { createEventDispatcher, onMount } from "svelte";
+  const dispatch = createEventDispatcher()
+
+  export let complete = false //compelete can use both as an event and as a bind
+  let elem
+  let children = []
+  
+  function setActiveChild() {
+    //Set every children inactive
+    for (let i = 0; i < children.length; i++) {
+      const child = children[i];
+      child.setAttribute('active', false)      
+    }
+
+    let allChildPassed = true
+    //Set the first non pass child active
+    for (let i = 0; i < children.length; i++) {
+      const child = children[i];
+      const pass = child.getAttribute('pass')
+      if ( pass != 'true' ) {
+        allChildPassed = false
+        child.setAttribute('active', true)
+        break
+      }
+    }
+
+    if (allChildPassed) {
+      if (!complete) {
+        complete = true
+        dispatch('complete')
+      }
+    } else {
+      complete = false
+    }
+  }
+
+  //TODO:
+  //Dispatch event when all children are passed
+  
+  onMount( () => {
+    children = elem.children
+    setActiveChild()
+    setInterval(setActiveChild, 50)
+  })
+  
+</script>
+
+<div bind:this={elem} id="poseCheckList">
+  <slot></slot>
+</div>
+
