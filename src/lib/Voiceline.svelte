479e48f8e827a7f067ff89949c5c994d16d63f25
vahvake <62074587+vahvake@users.noreply.github.com>
2023-04-21 14:23:37 +0300

    Add delete onDestroy and time threshold to Voiceline

--- src/lib/Voiceline.svelte
+++ src/lib/Voiceline.svelte
@@ -1,17 +1,28 @@
 <script>
   //Just and element access to the store
 
-  import { onMount } from "svelte"
-  import { SpeechQueue, addVoiceLine } from '@lib/speechQueue.js'
+  import { onDestroy, onMount } from "svelte"
+  import { SpeechQueue, addVoiceLine, deleteVoiceLine } from '@lib/speechQueue.js'
 
   export let txt = "Hello world"
+  let timeOutID //This is used to prevent adding the line with <50ms appearance
 
   onMount( () => {
     if ( !$SpeechQueue.lines.includes(txt) ) {
-      addVoiceLine(txt)
+      timeOutID = setTimeout( () => {
+        timeOutID = undefined
+        addVoiceLine(txt)
+      }, 50)
     }
   }) 
 
+  onDestroy( () => {
+    if (timeOutID) {
+      clearTimeout(timeOutID)
+    } else {
+      deleteVoiceLine(txt)
+    }
+  })
 
 </script>
 
