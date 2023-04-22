1aed59cfd6606b50cb516556824bff1b66a576b5
vahvake <62074587+vahvake@users.noreply.github.com>
2023-04-22 12:03:20 +0300

    Add check for deleted voicelines

--- src/lib/Voiceline.svelte
+++ src/lib/Voiceline.svelte
@@ -5,23 +5,15 @@
   import { SpeechQueue, addVoiceLine, deleteVoiceLine } from '@lib/speechQueue.js'
 
   export let txt = "Hello world"
-  let timeOutID //This is used to prevent adding the line with <50ms appearance
 
   onMount( () => {
     if ( !$SpeechQueue.lines.includes(txt) ) {
-      timeOutID = setTimeout( () => {
-        timeOutID = undefined
-        addVoiceLine(txt)
-      }, 50)
+      addVoiceLine(txt)
     }
   }) 
 
   onDestroy( () => {
-    if (timeOutID) {
-      clearTimeout(timeOutID)
-    } else {
-      deleteVoiceLine(txt)
-    }
+    deleteVoiceLine(txt)
   })
 
 </script>
